344431817e3429a39216cfd5f7563fff60366a5d
方启军 <2997855025@qq.com>
2024-07-09 20:33:08 +0800

    modified:   server.js

--- server.js
+++ server.js
@@ -407,6 +407,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/views', 'login.html'));
 });
 
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
